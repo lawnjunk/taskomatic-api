884cc9e9df2d55483eb041d803a9e9d42eee96f9
Duncan Marsh <slugbyte@slugbyte.com>
2020-05-25 16:18:30 +0800

    added profile routes
    * fetch and update own profile
    * retrive a list of all profiles

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -72,8 +72,11 @@
     * TODO: could use a redis channel to solve this problem
   * added update password and verify email routes
   * integration for new routes
+* 0.1.9 -- profile routes
+  * 
 
 ## Todo Roadmap
+* 0.1.10 -- mailer pub/sub with redis chanel
 * 0.1.? -- refactor
 * 0.1.? -- dockerize
 * 0.1.? -- continuous deployment
---
+++ src/route/profile.js
@@ -0,0 +1,40 @@
+'use strict'
+
+// external deps
+const debug = require('debug')
+const {Router} = require('express')
+const createError = require('http-errors')
+const jsonParser = require('body-parser').json()
+
+// internal modules
+const db = require('../lib/db.js')
+const User = require('../model/user.js')
+const bearer = require('../middleware/bearer-auth.js')
+const signing = require('../middleware/signing-middleare.js')
+
+// module constats
+const profileRouter = new Router()
+
+// interface
+profileRouter.get('/profile/self', bearer, signing, async (req, res) => {
+  debug('GET /pofile/self')
+  res.signJSON(JSON.parse(req.user.toSafeJSON()))
+})
+
+profileRouter.put('/profile/self', bearer, jsonParser, signing, async (req, res) => {
+  debug('PUT /pofile/self')
+  let result = await req.user.updateProfile(req.body)
+  res.signJSON(JSON.parse(result.toSafeJSON()))
+})
+
+profileRouter.get('/profile', bearer, signing, async (req, res) => {
+  debug('GET /profile')
+  let userKeys = await db.doit('keys', ['user:*'])
+  let userProfiles = await Promise.all(userKeys.map(async (key) => {
+    let user = await User.fetchByID(key)
+    return JSON.parse(user.toSafeJSON())
+  }))
+  res.signJSON(userProfiles)
+})
+
+module.exports = profileRouter
